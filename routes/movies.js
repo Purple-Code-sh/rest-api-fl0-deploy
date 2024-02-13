803a70eab7f5045c248a77a1cbc14d92c74a7ab1
Purple-Code-sh <purple.code.sh@gmail.com>
2024-02-12 18:26:05 -0600

    finish the convertion to MVC

--- routes/movies.js
+++ routes/movies.js
@@ -3,20 +3,10 @@ import { MovieController } from '../controlers/movies.js'
 
 export const moviesRouter = Router()
 
-// ------------- Get all movies ------------- .
 moviesRouter.get('/', MovieController.getAll)
-
-// ------------- Get a specific movie ------------- .
-moviesRouter.get('/:id', MovieController.getById)
-
-// ------------- Post a new movie ------------- .
 moviesRouter.post('/', MovieController.create)
 
-// ------------- Delete a specific movie ------------- .
+moviesRouter.get('/:id', MovieController.getById)
 moviesRouter.delete('/:id', MovieController.delete)
-
-// ------------- Update a specific movie ------------- .
 moviesRouter.patch('/:id', MovieController.update)
-
-// ------------- Options to solve CORS ------------- .
 moviesRouter.options('/:id', MovieController.corsOptions)
